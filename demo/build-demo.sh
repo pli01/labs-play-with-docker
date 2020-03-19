e038195d5e694656297bb6ad7bfb7978b62ebf4c
pli01 <pli01@noreply.github.com>
2020-03-19 17:49:54 +0100

    Add build/test/up

--- demo/build-demo.sh
+++ demo/build-demo.sh
@@ -2,5 +2,10 @@
 
 git clone -b develop https://github.com/pli01/candilibV2
 cd candilibV2
-make build-all
+time make build-all
+export FRONT_ADMIN_PORT=81
+export DBDATA=../test-db
 
+time make up-all
+time make test-all
+time make init-db-e2e
