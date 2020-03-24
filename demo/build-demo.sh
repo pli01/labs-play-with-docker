2a3e1db3f73bd19a36c954b575875e6825622720
pli01 <pli01@noreply.github.com>
2020-03-24 14:50:12 +0100

    branch develop

--- demo/build-demo.sh
+++ demo/build-demo.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-git clone -b develop https://github.com/pli01/candilibV2
+git clone -b develop https://github.com/LAB-MI/candilibV2
 cd candilibV2
 time make build-all
+
+exit 0
 export FRONT_ADMIN_PORT=81
 export DBDATA=../test-db
 
